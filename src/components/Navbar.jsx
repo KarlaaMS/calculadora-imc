969ac96b0f1a065bd40d9ae157801dc63c460824
KarlaaMS <salgadosanchez2000@gmail.com>
2026-06-24 21:43:04 -0500

    Quinto Commit - Algunas correcciones responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,8 +27,8 @@ function Navbar (){
       {abierto && (
         <ul className="nav-mobile">
           <li><a href="/#que-es-imc" onClick={() => setAbierto(false)}>¿Qué es IMC?</a></li>
-          <li><a href="/#calculo-mayores" onClick={() => setAbierto(false)}>IMC Adultos</a></li>
-          <li><a href="/#calculo-menores" onClick={() => setAbierto(false)}>IMC Menores</a></li>
+          <li><a href="/adultos" onClick={() => setAbierto(false)}>IMC Adultos</a></li>
+          <li><a href="/menores" onClick={() => setAbierto(false)}>IMC Menores</a></li>
           <li><a href="/#ayuda" onClick={() => setAbierto(false)}>Ayuda</a></li>
         </ul>
       )}
