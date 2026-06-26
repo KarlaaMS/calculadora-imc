073674afc622947d4746430bc3e0f5d368ac9b6d
KarlaaMS <salgadosanchez2000@gmail.com>
2026-06-26 16:01:29 -0500

    Sexto Commit - Coreccion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,10 +26,10 @@ function Navbar (){
       {/* Menú móvil */}
       {abierto && (
         <ul className="nav-mobile">
-          <li><a href="/#que-es-imc" onClick={() => setAbierto(false)}>¿Qué es IMC?</a></li>
+          <li><a href="/que-es-imc" onClick={() => setAbierto(false)}>¿Qué es IMC?</a></li>
           <li><a href="/adultos" onClick={() => setAbierto(false)}>IMC Adultos</a></li>
           <li><a href="/menores" onClick={() => setAbierto(false)}>IMC Menores</a></li>
-          <li><a href="/#ayuda" onClick={() => setAbierto(false)}>Ayuda</a></li>
+          <li><a href="#ayuda" onClick={() => setAbierto(false)}>Ayuda</a></li>
         </ul>
       )}
     </nav>
